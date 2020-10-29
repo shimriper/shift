9fc92c28232acab4a9f02ef4c4576069cb9c751b
shimriper <shimriper@gmail.com>
2020-10-29 12:37:41 +0200

    fix

--- env.js
+++ env.js
@@ -12,9 +12,10 @@ if (fs.existsSync('./public')) {
 } else {
     process.env.JWT = 'secret_this_should_be_longer'
     process.env.NODE_ENV = 'development';
-    process.env.databaseUri = 'mongodb://localhost:27017/mish_req'; // Databse URI and database name
-    // process.env.databaseUri = 'mongodb://shift:!QAZ2wsx@ds048279.mlab.com:48279/shifts2020'; // Databse URI and database name
-    process.env.databaseName = 'development database: mish_req'; // Database name
+    // process.env.databaseUri = 'mongodb://localhost:27017/mish_req'; // Databse URI and database name
+    process.env.databaseUri = 'mongodb://shift:!QAZ2wsx@ds048279.mlab.com:48279/shifts2020'; // Databse URI and database name
+    process.env.databaseName = 'development database: shifts2020'; // Database name
+    // process.env.databaseName = 'development database: mish_req'; // Database name
     process.env.GMAIL_USER = "shiftboishift@gmail.com";
     process.env.GMAIL_PWS = "Cbehartk1!";
     process.env.apiKey = '37237b83';
